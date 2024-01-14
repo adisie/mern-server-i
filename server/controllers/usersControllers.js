8564205338e97db217cc88f2dc18d845583632f2
Adisie Fenta <adisiefenta23@gmail.com>
2024-01-14 07:48:04 +0300

    some changes on the backend

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,23 +1,84 @@
+const bcryptjs = require('bcryptjs')
 
 // models
 // user model
 const User = require('../models/usersModel')
 
+// utils
+const {
+    MAX_AGE,
+    errorHandler,
+    generateToken,
+} = require('../utils/usersUtils')
+
 // signup
-const signup = (req,res) => {
+const signup = async (req,res) => {
     try{
         const {username,email,password} = req.body 
-        res.status(200).json({username,email,password})
-    }catch(err){
-        res.status(400).json({
-            error: 'signup error'
+        const user = await User.create({username,email,password})
+        // generate token 
+        const token = generateToken(user._id)
+        // set cookie
+        res.cookie('token',token,{
+            maxAge: MAX_AGE * 1000,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true
         })
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                username: user.username
+            }
+        })
+    }catch(err){
+        const errors = errorHandler(err)
+        res.status(400).json({errors})
     }
 }
 
 // login
-const login = (req,res) => {
-    res.status(200).json('LOGIN')
+const login = async (req,res) => {
+    try{
+        const {username,password} = req.body 
+        // username checker
+        if(!username?.trim()){
+            throw new Error('username required')
+        }
+        // password checker
+        if(!password){
+            throw new Error('password required')
+        }
+        // find user
+        const user = await User.findOne({username})
+        // check is user exist
+        if(!user){
+            throw new Error('username not exist')
+        }
+        // check if password is correct
+        const isPassMatch = bcryptjs.compareSync(password,user.password)
+        if(!isPassMatch){
+            throw new Error('incorrect password')
+        }
+        // generate token
+        const token = generateToken(user._id)
+        // set cookie
+        res.cookie('token',token,{
+            maxAge: MAX_AGE * 1000,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+        })
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                username: user.username,
+            }
+        })
+    }catch(err){
+        const errors = errorHandler(err)
+        res.status(200).json({errors})
+    }
 }
 
 // logout
