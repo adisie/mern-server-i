4ad62d18701c53861068a1f5203f0870fd8ddff0
Adisie Fenta <adisiefenta23@gmail.com>
2024-01-16 08:28:10 +0300

    some changes on the backend

--- server/controllers/postsControllers.js
+++ server/controllers/postsControllers.js
@@ -3,18 +3,72 @@
 const Post = require('../models/postsModel')
 
 // get all posts
-const getAllPosts = (req,res) => {
-    res.status(200).json('GET ALL POSTS')
+const getAllPosts = async (req,res) => {
+    try{
+        const posts = await Post.find().sort({createdAt: -1}).select({
+            _id: 1,
+            authorId: 1,
+            text: 1,
+            createdAt: 1
+        })
+        res.status(200).json({
+            posts
+        })
+    }catch(err){
+        res.status(400).json({
+            error: 'get all posts error'
+        })
+    }
 }
 
 // add new post
-const addNewPost = (req,res) => {
-    res.status(200).json("ADD NEW POST")
+const addNewPost = async (req,res) => {
+    try{
+        const {text} = req.body 
+        const newPost = await Post.create({
+            authorId: req.user._id,
+            text,
+        })
+        res.status(200).json({
+            post: {
+                _id: newPost._id,
+                authorId: newPost.authorId,
+                text: newPost.text,
+                createdAt: newPost.createdAt,
+            }
+        })
+    }catch(err){
+        res.status(400).json({
+            error: 'add new post error'
+        })
+    }
 }
 
 // delete post
-const deleteSinglePost = (req,res) => {
-    res.status(200).json('DELETE POST')
+const deleteSinglePost = async (req,res) => {
+    try{
+        const {_id} = req.params 
+        const post = await Post.findById(_id)
+        if(!post) {
+            return res.status(400).json({
+                error: 'post not exist'
+            })
+        }
+        if(post.authorId.toString() !== req.user._id.toString()){
+            return res.status(400).json({
+                error: 'unauthorized to delete other\'s post'
+            })
+        }
+        await post.deleteOne()
+        res.status(200).json({
+            message: 'the post is deleted',
+            _id,
+        })
+    }catch(err){
+        res.status(400).json({
+            error: 'delete single post error'
+        })
+    }
 }
 
 
